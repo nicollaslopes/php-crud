9245dbfb5bdfb2ab9e00341fa69886a63d61c32e
nicollaslopes <nicollaslopesn@gmail.com>
2021-03-17 21:02:39 -0300

    arquivo sql adicionado

--- db/db_adm.sql
+++ db/db_adm.sql
@@ -1,5 +1,5 @@
 CREATE TABLE administrador(
-	idadministrador INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	id_administrador INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     login VARCHAR (30) NOT NULL,
     senha VARCHAR (255) NOT NULL
 )
--- db/db_func.sql
+++ db/db_func.sql
@@ -1,7 +1,7 @@
 CREATE TABLE funcionario(
-	idfuncionario INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	id_funcionario INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     nome VARCHAR (100) NOT NULL,
     salario INT(10) NOT NULL,
-    dtNascimento DATE,
+    dt_nascimento DATE,
     cargo VARCHAR(40)
 )
